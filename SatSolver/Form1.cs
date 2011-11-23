13a402768accfb88b5ecf0468a5a09a128908f44
Sergei Popov <sergppv@gmail.com>
2011-11-23 09:53:17 +0300

    Refactor border satisfability

--- SatSolver/Form1.cs
+++ SatSolver/Form1.cs
@@ -29,12 +29,12 @@ namespace SatSolver
             backgroundWorker = new BackgroundWorker();
             backgroundWorker.WorkerSupportsCancellation = true;
             backgroundWorker.WorkerReportsProgress = true;
-            backgroundWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(_backgroundWorker_RunWorkerCompleted);
-            backgroundWorker.ProgressChanged += new ProgressChangedEventHandler(_backgroundWorker_ProgressChanged);
+            backgroundWorker.RunWorkerCompleted += BackgroundWorkerRunWorkerCompleted;
+            backgroundWorker.ProgressChanged += BackgroundWorkerProgressChanged;
 
             if (tabControl1.SelectedIndex == 0)
             {
-                backgroundWorker.DoWork += new DoWorkEventHandler(_backgroundWorker_DoWork);
+                backgroundWorker.DoWork += BackgroundWorkerDoWork;
 
                 variableCount = (int)numMembersCount.Value;
                 iKonyncCount = (int)numKonyncCount.Value;
@@ -44,7 +44,7 @@ namespace SatSolver
             }
             else
             {
-                backgroundWorker.DoWork += new DoWorkEventHandler(backgroundWorker_DoWork_BorderSatisfiability);
+                backgroundWorker.DoWork += BackgroundWorkerDoWorkBorderSatisfiability;
 
                 variableCount = (int)numBorderVariableCount.Value;
                 iFreeMembers = (int)numFreeMembers.Value;
@@ -59,7 +59,7 @@ namespace SatSolver
             btnStopCalculate.Enabled = true;
         }
 
-        void _backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        void BackgroundWorkerDoWork(object sender, DoWorkEventArgs e)
         {
             var experimentResult = new ExperimentResult
                                        {
@@ -95,8 +95,8 @@ namespace SatSolver
                 var buffer = new byte[BinaryCounter.BinaryCounter.FindSizeBitMapForCountParams(countParams)];
                 Log.DebugFormat("Size of mass = {0}", buffer.Length);
                 
-                int[] source = BinaryCounter.BinaryCounter.FindFreeMembersIndex(countParams, countFreeMembers);
-                uint mask = BinaryCounter.BinaryCounter.GetMask(source);
+                int[] freeMembersIndex = BinaryCounter.BinaryCounter.FindFreeMembersIndex(countParams, countFreeMembers);
+                uint mask = BinaryCounter.BinaryCounter.GetMask(freeMembersIndex);
                 uint rotatedMask = BinaryCounter.BinaryCounter.GetRotateMask(mask);
                 Log.DebugFormat("Mask = {0}", mask);
                 Log.DebugFormat("RotateMask = {0}", rotatedMask);
@@ -132,13 +132,11 @@ namespace SatSolver
                     e.Cancel = true;
                     break;
                 }
-
-
             }
             e.Result = experimentResult;
         }
 
-        void backgroundWorker_DoWork_BorderSatisfiability(object sender, DoWorkEventArgs e)
+        void BackgroundWorkerDoWorkBorderSatisfiability(object sender, DoWorkEventArgs e)
         {
             var borderExperimentResult = new BorderExperimentResult();
             borderExperimentResult.VariableCount = variableCount;
@@ -155,67 +153,42 @@ namespace SatSolver
 
             for (int experimentIndex = 0; (experimentIndex < iExperimentRepeat) && (!e.Cancel); experimentIndex++)
             {
-                int num11;
                 Log.Debug("------------------Next generation");
-                //int num = _iKonyncCount;
-                int iVariableCount = this.variableCount;
-                int num3 = iFreeMembers;
-                uint num4 = 0;
-                int num6 = -1;
-                int num7 = -1;
-                int[] source = new int[num3];
-                int num8 = ((1) << iVariableCount) / 8;
-                if (num8 == 0)
-                {
-                    num8++;
-                }
-                byte[] buffer = new byte[num8];
-                Log.Debug("Size of mass = " + num8);
-                int index = 0;
-                while (index < num3)
-                {
-                    int num10 = rnd.Next() % iVariableCount;
-                    if (!source.Contains<int>(num10))
-                    {
-                        source[index] = num10;
-                        index++;
-                    }
-                }
-                for (num11 = 0; num11 < num3; num11++)
-                {
-                    int num12 = 0;
-                    num12 = ((int)1) << source[num11];
-                    num6 ^= num12;
-                }
-                num7 ^= num6;
-                Log.Debug("Mask = " + num6);
-                Log.Debug("RotateMask = " + num7);
+                int num = iKonyncCount;
+                int countParams = variableCount;
+                int countFreeMembers = iFreeMembers;
+                uint countUniqueConjunction = 0;
+
+                var buffer = new byte[BinaryCounter.BinaryCounter.FindSizeBitMapForCountParams(countParams)];
+                Log.DebugFormat("Size of mass = {0}", buffer.Length);
+
+                int[] freeMembersIndex = BinaryCounter.BinaryCounter.FindFreeMembersIndex(countParams, countFreeMembers);
+                uint mask = BinaryCounter.BinaryCounter.GetMask(freeMembersIndex);
+                uint rotatedMask = BinaryCounter.BinaryCounter.GetRotateMask(mask);
+                Log.DebugFormat("Mask = {0}", mask);
+                Log.DebugFormat("RotateMask = {0}", rotatedMask);
 
                 float currentSatisfiability = 0;
                 int indexMinterm = 0;
                 do
                 {
-                    int num5 = rnd.Next() % (1 << iVariableCount);
-                    Log.Debug("Generate Digit = " + num5);
-                    int num13 = 0;
-                    for (int i = 0; i < (1 << num3); i++)
+                    uint generatedConjunction = BinaryCounter.BinaryCounter.GetRandomСonjunction(countParams);
+                    Log.DebugFormat("Generate Digit = {0}", generatedConjunction);
+
+                    foreach (var recoveredConjunction in BinaryCounter.BinaryCounter.RecoveredConjunction(mask, rotatedMask, generatedConjunction, countFreeMembers))
                     {
-                        num13 |= num6;
-                        num13++;
-                        num13 &= num7;
-                        int num14 = num5 ^ num13;
-                        int num16 = num14 / 8;
-                        Log.Debug("                PereborDigit = " + num14);
-                        Log.Debug("                Index = " + num16);
-                        byte num17 = Convert.ToByte((int)(((int)1) << (num14 % 8)));
-                        if ((buffer[num16] & num17) == 0)
-                        {
-                            num4++;
-                        }
-                        Log.Debug("                Count = " + num4);
-                        buffer[num16] = (byte)(buffer[num16] | Convert.ToByte((int)(((int)1) << (num14 % 8))));
+                        uint byteIndex = recoveredConjunction / 8;
+                        Log.Debug("                PereborDigit = " + recoveredConjunction);
+                        Log.Debug("                Index = " + byteIndex);
+
+                        byte conjunctionByte = BinaryCounter.BinaryCounter.GetCheckedBitrForConjunction(recoveredConjunction);
+                        if ((buffer[byteIndex] & conjunctionByte) == 0)
+                            countUniqueConjunction++;
+
+                        Log.Debug("                Count = " + countUniqueConjunction);
+                        buffer[byteIndex] |= conjunctionByte;
                     }
-                    currentSatisfiability = Convert.ToSingle(num4) / ((float)((1) << this.variableCount));
+                    currentSatisfiability = Convert.ToSingle(countUniqueConjunction) / (1 << variableCount);
                     indexMinterm++;
 
                     Log.Debug("Total = " + currentSatisfiability);
@@ -227,7 +200,7 @@ namespace SatSolver
 
                 backgroundWorker.ReportProgress(experimentIndex);
 
-                if (backgroundWorker.CancellationPending == true)
+                if (backgroundWorker.CancellationPending)
                 {
                     e.Cancel = true;
                     break;
@@ -236,12 +209,12 @@ namespace SatSolver
             e.Result = borderExperimentResult;
         }
 
-        void _backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        void BackgroundWorkerProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            progressBar1.Value = (int)e.ProgressPercentage;
+            progressBar1.Value = e.ProgressPercentage;
         }
 
-        void _backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        void BackgroundWorkerRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
             EnableControls();
             btnStopCalculate.Enabled = false;
@@ -259,14 +232,14 @@ namespace SatSolver
             {
                 if (tabControl1.SelectedIndex == 0)
                 {
-                    ExperimentResult experimentResult = (ExperimentResult)e.Result;
-                    ReportForm reportForm = new ReportForm(experimentResult);
+                    var experimentResult = (ExperimentResult)e.Result;
+                    var reportForm = new ReportForm(experimentResult);
                     reportForm.Show();
                 }
                 else
                 {
-                    BorderExperimentResult borderExperimentResult = (BorderExperimentResult)e.Result;
-                    ReportBorderForm reportBorderForm = new ReportBorderForm(borderExperimentResult);
+                    var borderExperimentResult = (BorderExperimentResult)e.Result;
+                    var reportBorderForm = new ReportBorderForm(borderExperimentResult);
                     reportBorderForm.Show();
                 }
             }
@@ -275,7 +248,7 @@ namespace SatSolver
 
         private void DisableControls()
         {
-            foreach (Control item in this.Controls)
+            foreach (Control item in Controls)
             {
                 item.Enabled = false;
             }
@@ -285,7 +258,7 @@ namespace SatSolver
 
         private void EnableControls()
         {
-            foreach (Control item in this.Controls)
+            foreach (Control item in Controls)
             {
                 item.Enabled = true;
             }
